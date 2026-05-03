76f7f7af21d4576dbdb07c6e303102bc23962a70
lixingcong <lixingcong@live.com>
2026-05-03 12:39:13 +0800

    add clipboard plugin

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,100 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {App, Editor, MarkdownFileInfo, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
+import {DEFAULT_SETTINGS, ClipboardReplaceSettings, ClipboardReplaceSettingTab, ReplaceMode} from "./settings";
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+type CustomJsHook = (srcText: string) => string;
+
+export default class ClipboardReplacePlugin extends Plugin {
+	settings: ClipboardReplaceSettings | undefined;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		let noticeText = 'Lxc clipboard reaplce';
+		if (ReplaceMode.None == this.settings!.replaceMode)
+			noticeText += ': Please setup first';
+
+		new Notice(noticeText);
+
+		// Ensure a function retains its context (the this value)
+		// when it is passed as a reference or called in a different scope.
+		const boundedPaste = this.paste.bind(this);
+
+		this.addCommand({
+			id: 'paste',
+			name: 'Paste',
+			editorCallback: boundedPaste
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
+		this.addRibbonIcon('presentation', 'Lxc clipboard paste', async () => {
+			const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if(markdownView)
+				await this.paste(markdownView.editor, markdownView);
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+		this.addSettingTab(new ClipboardReplaceSettingTab(this.app, this));
 	}
 
 	onunload() {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<ClipboardReplaceSettings>);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	async paste(editor: Editor, ctx: MarkdownView | MarkdownFileInfo) {
+		switch(this.settings!.replaceMode){
+			case ReplaceMode.Simple:
+			case ReplaceMode.Regex: {
+				const isRegex = ReplaceMode.Regex == this.settings!.replaceMode;
+				await navigator.clipboard.readText().then(clipboard => {
+					if(this.settings!.normalReplaceParam.from.length == 0 || clipboard.length == 0){
+						new Notice('Paste failed, either the clipboard or search for text is empty');
+						return;
+					}
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+					const reFrom = isRegex ? new RegExp(this.settings!.normalReplaceParam.from) : this.settings!.normalReplaceParam.from;
+					const reTo = this.settings!.normalReplaceParam.to;
+					const newText = clipboard.replace(reFrom, reTo);
+					editor.replaceSelection(newText);
+				});
+				break;
+			}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+			case ReplaceMode.Custom:{
+				// eslint-disable-next-line no-eval
+				const f = eval(this.settings!.customReplaceParam.code) as CustomJsHook;
+				if(undefined != f){
+					await navigator.clipboard.readText().then(clipboard => {
+						if(clipboard.length == 0){
+							new Notice('Paste failed, the clipboard is empty');
+							return;
+						}
+
+						const newText = f(clipboard);
+						if(typeof newText === 'string')
+							editor.replaceSelection(newText);
+						else {
+							// eslint-disable-next-line obsidianmd/ui/sentence-case
+							new Notice('Please return string in your javascript code');
+						}
+					});
+				}else{
+					// eslint-disable-next-line obsidianmd/ui/sentence-case
+					new Notice('Please check your syntax of javascript code');
+				}
+				break;
+			}
+
+			default:
+				new Notice('Paste failed, please setup first!');
+				break;
+		}
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,109 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import ClipboardReplacePlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export enum ReplaceMode {
+	None = 'none',
+	Simple = 'simple',
+	Regex = 'regex',
+	Custom = 'custom'
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export interface NormalReplaceParam {
+	from: string,
+	to: string
 }
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export interface CustomReplaceParam {
+	code: string
+}
 
-	constructor(app: App, plugin: MyPlugin) {
+export interface ClipboardReplaceSettings {
+	replaceMode: ReplaceMode,
+	normalReplaceParam: NormalReplaceParam,
+	customReplaceParam: CustomReplaceParam
+}
+
+export const DEFAULT_SETTINGS: ClipboardReplaceSettings = {
+	replaceMode: ReplaceMode.None,
+	normalReplaceParam: {
+		from: '',
+		to: ''
+	},
+	customReplaceParam: {
+		code: '(srcText) => {\n  const newText = srcText + \'aaa\';\n  return newText;\n}'
+	}
+}
+
+export class ClipboardReplaceSettingTab extends PluginSettingTab {
+	plugin: ClipboardReplacePlugin;
+
+	constructor(app: App, plugin: ClipboardReplacePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Mode')
+			.addDropdown(dropDown => {
+				dropDown
+					.addOption(ReplaceMode.None, 'None')
+					.addOption(ReplaceMode.Simple, 'Simple replace')
+					.addOption(ReplaceMode.Regex, 'Regex replace')
+					.addOption(ReplaceMode.Custom, 'Custom script')
+					.setValue(this.plugin.settings!.replaceMode)
+					.onChange(async (value) => {
+						this.plugin.settings!.replaceMode = value as ReplaceMode;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName('Simple/regex search for')
+			.addText(text => {
+				text
+					.setValue(this.plugin.settings!.normalReplaceParam.from)
+					.onChange(async (value) => {
+						this.plugin.settings!.normalReplaceParam.from = value;
+						await this.plugin.saveSettings();
+					})
+					.then(cb => {
+						cb.inputEl.classList.add('full-width')
+					});
+			});
+
+		new Setting(containerEl)
+			.setName('Simple/regex repalce to')
+			.addText(text => {
+				text
+					.setValue(this.plugin.settings!.normalReplaceParam.to)
+					.onChange(async (value) => {
+						this.plugin.settings!.normalReplaceParam.to = value;
+						await this.plugin.saveSettings();
+					})
+					.then(cb => {
+						cb.inputEl.classList.add('full-width')
+					});
+			});
+
+		new Setting(containerEl)
+			.setName('Custom js')
+			.setDesc('Make sure your code is correct')
+			.addTextArea(text => {
+				text
+					.setValue(this.plugin.settings!.customReplaceParam.code)
+					.onChange(async (value) => {
+						this.plugin.settings!.customReplaceParam.code = value;
+						await this.plugin.saveSettings();
+					})
+					.then(cb => {
+						cb.inputEl.classList.add('full-width')
+						cb.inputEl.rows = 10;
+					});
+			});
 	}
 }
